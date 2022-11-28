8a11519deb3286ef4ed09f2ac9285f475acc2801
OlgaRyzha <orizha11@gmail.com>
2022-11-28 06:28:37 +0200

    додала бай нав

--- src/js/modal-buy-now.js
+++ src/js/modal-buy-now.js
@@ -1,18 +1,14 @@
-// (() => {
-//   const refs = {
-//     openModalBtn1: document.querySelector("[data-modal-open-buy-now-mob]"),
-//     openModalBtn2: document.querySelector("[data-modal-open-buy-now-tablet]"),
-//     openModalBtn3: document.querySelector("[data-modal-open-buy-now-desk]"),
-//     closeModalBtn: document.querySelector("[data-modal-close-buy-now]"),
-//     modal: document.querySelector("[data-modal-buy-now]"),
-//   };
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[header-data-modal-open]"),
+    closeModalBtn: document.querySelector("[header-data-modal-close]"),
+    modal: document.querySelector("[header-data-modal]"),
+  };
 
-//   refs.openModalBtn1.addEventListener("click", toggleModal);
-//   refs.openModalBtn2.addEventListener("click", toggleModal);
-//   refs.openModalBtn3.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden-buy-now");
-//   }
-// })();
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
